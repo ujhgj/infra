9ae76a100f6d8b6d56e9a2234f68607810dd7e75
Alex Kovytin <kovytin.alexander@gmail.com>
2017-09-14 17:46:58 +0300

    +packer образы отдельно для бд и апп +tf модули Все работает, только у аппа нет коннекта с бд

---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,6 @@
+variable public_key_path {
+  description = "Path to the public key used for ssh access"
+}
+variable db_disk_image {
+  description = "Disk image for Reddit database server"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,19 @@
 variable project {
   description = "Project ID"
 }
-
 variable region {
   description = "Region"
   default     = "europe-west1"
 }
-
 variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
-
 variable provisioner_connection_key_path {
   description = "Path to the private key used for provisioner's ssh access"
 }
-
-variable disk_image {
-  description = "Disk image"
+variable app_disk_image {
+  description = "Disk image for Reddit app server"
+}
+variable db_disk_image {
+  description = "Disk image for Reddit database server"
 }
